f7a85c1a4a85899f2cc9c72b4bf0921518d5c534
Vincent <vincentb45@gmail.com>
2023-01-24 23:43:29 +0100

    Add kinduff api to display dog facts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
+import { Inter } from '@next/font/google'
 import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
@@ -7,12 +10,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   )
 }
